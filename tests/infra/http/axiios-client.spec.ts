5ba485913b3f9d902bab5a7d940a47358ec84152
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-19 23:21:59 -0300

    isolate SUT creation

--- tests/infra/http/axiios-client.spec.ts
+++ tests/infra/http/axiios-client.spec.ts
@@ -1,4 +1,5 @@
 import { type HttpGetClient } from '@/infra/http'
+
 import axios from 'axios'
 
 jest.mock('axios')
@@ -12,23 +13,27 @@ class AxiosHttpClient {
 }
 
 describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
+
   describe('get', () => {
     it('should call get with correct params', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
+      await sut.get({ url, params })
 
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
-      })
-
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any'
-        }
-      })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
   })
