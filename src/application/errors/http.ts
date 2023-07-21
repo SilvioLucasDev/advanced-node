4fee3610f57822d1c5744e353b8597f524403fe2
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-21 00:51:21 -0300

    apply badRequest helper

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -1,7 +1,14 @@
 export class ServerError extends Error {
   constructor (error?: Error | undefined) {
     super('Server failed. Try again soon')
-    this.name = 'Server Error'
+    this.name = 'ServerError'
     this.stack = error?.stack
+  }
+}
+
+export class RequiredFieldError extends Error {
+  constructor (fieldName: string) {
+    super(`The field ${fieldName} is required`)
+    this.name = 'RequiredFieldError'
   }
 }
