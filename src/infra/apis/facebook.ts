510008c8cdf3b6a1f403e92a417dae21015b5695
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-22 16:16:39 -0300

    change returns of FacebookApi

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -37,7 +37,7 @@ export class FacebookApi implements LoadFacebookUserApi {
   }
 
   private async getAppToken (): Promise<AppToken> {
-    return this.httpClient.get({
+    const token = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
@@ -45,29 +45,35 @@ export class FacebookApi implements LoadFacebookUserApi {
         grant_type: 'client_credentials'
       }
     })
+
+    return token.data
   }
 
   private async getDebugToken (clientToken: string): Promise<DebugToken> {
     const appToken = await this.getAppToken()
 
-    return this.httpClient.get({
-      url: `${this.baseUrl}/oauth/debug_token`,
+    const userId = await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
         input_token: clientToken
       }
     })
+
+    return userId.data
   }
 
   private async getUserInfo (clientToken: string): Promise<UserInfo> {
     const debugToken = await this.getDebugToken(clientToken)
 
-    return this.httpClient.get({
+    const userInfo = await this.httpClient.get({
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
         fields: ['id', 'name', 'email'].join(','),
         access_token: clientToken
       }
     })
+
+    return userInfo.data
   }
 }
--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -18,12 +18,14 @@ describe('FacebookApi', () => {
 
   beforeEach(() => {
     httpClient.get
-      .mockResolvedValueOnce({ access_token: 'any_app_token' })
-      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ data: { access_token: 'any_app_token' } })
+      .mockResolvedValueOnce({ data: { data: { user_id: 'any_user_id' } } })
       .mockResolvedValueOnce({
-        id: 'any_fb_id',
-        name: 'any_fb_name',
-        email: 'any_fb_email'
+        data: {
+          id: 'any_fb_id',
+          name: 'any_fb_name',
+          email: 'any_fb_email'
+        }
       })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
@@ -45,7 +47,7 @@ describe('FacebookApi', () => {
     await sut.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: 'https://graph.facebook.com/oauth/debug_token',
+      url: 'https://graph.facebook.com/debug_token',
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
