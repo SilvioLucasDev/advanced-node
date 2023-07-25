a24ec40207e55507b9348feb6f6a2fd90ca7d8aa
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-25 01:26:37 -0300

    move ExpressMiddleware to its own file

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -1,26 +1,9 @@
-import { type HttpResponse } from '@/application/helpers'
-import { getMockReq, getMockRes } from '@jest-mock/express'
+import { type Middleware } from '@/application/middlewares'
+import { adaptExpressMiddleware } from '@/main/adapters/express-middleware'
+
 import { type Request, type Response, type RequestHandler, type NextFunction } from 'express'
+import { getMockReq, getMockRes } from '@jest-mock/express'
 import { type MockProxy, mock } from 'jest-mock-extended'
-
-type Adapter = (middleware: Middleware) => RequestHandler
-
-const adaptExpressMiddleware: Adapter = (middleware) => {
-  return async (req, res, next) => {
-    const { statusCode, data } = await middleware.handle({ ...req.headers })
-    if (statusCode === 200) {
-      const entries = Object.entries(data).filter(entry => entry[1])
-      req.locals = { ...req.locals, ...Object.fromEntries(entries) }
-      next()
-    } else {
-      res.status(statusCode).json(data)
-    }
-  }
-}
-
-interface Middleware {
-  handle: (httpRequest: any) => Promise<HttpResponse>
-}
 
 describe('ExpressMiddleware', () => {
   let req: Request
