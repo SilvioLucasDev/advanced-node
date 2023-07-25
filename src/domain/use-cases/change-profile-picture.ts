33a3c130b6d0249c678701cfb074d0d96b8571d8
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-25 14:51:13 -0300

    move production code to their own files

---
+++ src/domain/use-cases/change-profile-picture.ts
@@ -0,0 +1,12 @@
+import { type UploadFile, type UUIDGenerator } from '@/domain/contracts/gateways'
+
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
+
+type Input = { id: string, file: Buffer }
+export type ChangeProfilePicture = (input: Input) => Promise<void>
+
+export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => {
+  return async ({ id, file }) => {
+    await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
+  }
+}
--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,32 +1,7 @@
+import { setupChangeProfilePicture, type ChangeProfilePicture } from '@/domain/use-cases/change-profile-picture'
+import { type UUIDGenerator, type UploadFile } from '@/domain/contracts/gateways'
+
 import { type MockProxy, mock } from 'jest-mock-extended'
-
-type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
-
-type Input = { id: string, file: Buffer }
-type ChangeProfilePicture = (input: Input) => Promise<void>
-
-const setupChangeProfilePicture: Setup = (fileStorage, crypto) => {
-  return async ({ id, file }) => {
-    await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
-  }
-}
-
-interface UploadFile {
-  upload: (input: UploadFile.Input) => Promise<void>
-}
-
-namespace UploadFile {
-  export type Input = { file: Buffer, key: string }
-}
-
-interface UUIDGenerator {
-  uuid: (input: UUIDGenerator.Input) => UUIDGenerator.Output
-}
-
-namespace UUIDGenerator {
-  export type Input = { key: string }
-  export type Output = string
-}
 
 describe('ChangeProfilePicture', () => {
   let uuid: string
