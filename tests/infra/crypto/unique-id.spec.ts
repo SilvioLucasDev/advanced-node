591fca0e229faa0d47e516a34986d6cd30b05c24
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-25 23:37:23 -0300

    ensure UniqueId returns correct uuid

---
+++ tests/infra/crypto/unique-id.spec.ts
@@ -0,0 +1,17 @@
+describe('UniqueId', () => {
+  it('should return correct uuid', () => {
+    const sut = new UniqueId(new Date(2023, 9, 3, 10, 10, 10))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_20231003101010')
+  })
+
+  it('should return correct uuid', () => {
+    const sut = new UniqueId(new Date(2018, 2, 10, 18, 1, 0))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_20180310180100')
+  })
+})
