8b7236776425e9fd3e792fa557770ddfa67116d9
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-27 17:01:17 -0300

    ensure MulterAdapter returns 500 if upload fails

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -1,19 +1,24 @@
+import { ServerError } from '@/application/errors'
+
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { type Request, type Response, type RequestHandler, type NextFunction } from 'express'
+
 import multer from 'multer'
 
 jest.mock('multer')
 
 const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture')
-  upload(req, res, () => {})
+  upload(req, res, error => {
+    res.status(500).json({ error: new ServerError(error).message })
+  })
 }
 
 describe('MulterAdapter', () => {
-  let fakeMulter: jest.Mocked<typeof multer>
   let uploadSpy: jest.Mock
   let singleSpy: jest.Mock
   let multerSpy: jest.Mock
+  let fakeMulter: jest.Mocked<typeof multer>
   let req: Request
   let res: Response
   let next: NextFunction
@@ -21,10 +26,10 @@ describe('MulterAdapter', () => {
 
   beforeAll(() => {
     fakeMulter = multer as jest.Mocked<typeof multer>
-    uploadSpy = jest.fn()
+    uploadSpy = jest.fn().mockImplementation(() => {})
     singleSpy = jest.fn().mockImplementation(() => uploadSpy)
     multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }))
-    jest.mocked(fakeMulter).mockImplementationOnce(multerSpy)
+    jest.mocked(fakeMulter).mockImplementation(multerSpy)
     req = getMockReq()
     res = getMockRes().res
     next = getMockRes().next
@@ -43,5 +48,19 @@ describe('MulterAdapter', () => {
     expect(singleSpy).toHaveBeenCalledTimes(1)
     expect(uploadSpy).toHaveBeenCalledWith(req, res, expect.any(Function))
     expect(uploadSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 500 if upload fails', () => {
+    const error = new Error('multer_error')
+    uploadSpy.mockImplementationOnce((req, res, next) => {
+      next(error)
+    })
+
+    sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: new ServerError(error).message })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
