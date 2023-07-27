341d71a35f0ca439a5c8627546064c4ad2e0b0e0
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-27 18:06:55 -0300

    ensure PUT /users/picture returns 403 is no authorization header is present

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -6,4 +6,5 @@ import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adapt(makeSavePictureController()))
+  router.put('/users/picture', auth)
 }
