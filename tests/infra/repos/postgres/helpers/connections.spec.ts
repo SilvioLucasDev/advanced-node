07aae8c3190a6215ecc1b92cb872ce6aea1bc1ba
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-29 02:16:29 -0300

    ensure PgConnection commit transaction

--- tests/infra/repos/postgres/helpers/connections.spec.ts
+++ tests/infra/repos/postgres/helpers/connections.spec.ts
@@ -20,6 +20,7 @@ describe('PgConnection', () => {
   let closeSpy: jest.Mock
   let startTransactionSpy: jest.Mock
   let releaseSpy: jest.Mock
+  let commitTransactionSpy: jest.Mock
   let sut: PgConnection
 
   beforeAll(() => {
@@ -31,9 +32,11 @@ describe('PgConnection', () => {
 
     startTransactionSpy = jest.fn()
     releaseSpy = jest.fn()
+    commitTransactionSpy = jest.fn()
     createQueryRunnerSpy = jest.fn().mockReturnValue({
       startTransaction: startTransactionSpy,
-      release: releaseSpy
+      release: releaseSpy,
+      commitTransaction: commitTransactionSpy
     })
 
     createConnectionSpy = jest.fn().mockResolvedValue({
@@ -126,6 +129,23 @@ describe('PgConnection', () => {
     const promise = sut.closeTransaction()
 
     expect(releaseSpy).not.toHaveBeenCalled()
+    await expect(promise).rejects.toThrow(new ConnectionNotFoundError())
+  })
+
+  it('should commit transaction', async () => {
+    await sut.connect()
+    await sut.commit()
+
+    expect(commitTransactionSpy).toHaveBeenCalledWith()
+    expect(commitTransactionSpy).toHaveBeenCalledTimes(1)
+
+    await sut.disconnect()
+  })
+
+  it('should return ConnectionNotFoundError on commit if connection is not found', async () => {
+    const promise = sut.commit()
+
+    expect(commitTransactionSpy).not.toHaveBeenCalled()
     await expect(promise).rejects.toThrow(new ConnectionNotFoundError())
   })
 })
